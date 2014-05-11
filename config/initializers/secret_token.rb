f1703f072c37be62fb17e849f3203e2fc510d391
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2014-05-11 03:38:17 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WayfarerLive::Application.config.secret_key_base = 'f0189ae092525fc1c214475b287981e10f8e207aad21d4413029505dd5c6dc6fc8b2379a52ec096c8fa33eacd34754eff50b8646086e3ec14b07e11738ed1374'
